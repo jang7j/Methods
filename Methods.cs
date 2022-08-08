efaa3160370ffe044f27a232a1a1b3eab2b56efa
Yun Jang <jang7j@yahoo.com>
2022-08-08 11:48:06 -0500

    initial commit

---
+++ Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+    public static int Multiply(int x, int y)
+        {
+            return x * y; 
+        }
+    public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    public static int Modulus(int x, int y)
+        {
+            return x % y;
+        }
+    }
+
+}
+
+
